9c6051147ed3e6999f3c422df8d51c34887db030
mgoel283 <mgoel283@gmail.com>
2018-06-07 14:59:32 -0400

    changes to firststep

--- firststep.py
+++ firststep.py
@@ -1,13 +1,18 @@
 #trying to write to thread safe queue
 import pyaudio
 import queue
+#import wave
+import numpy
+import matplotlib.pyplot as plt
 
 CHUNK = 1024
 WIDTH = 2
 CHANNELS = 2
 RATE = 44100
-RECORD_SECONDS = 1
+#RECORD_SECONDS = 1
 FORMAT = pyaudio.paInt16
+WAVE_OUTPUT_FILENAME = "output.wav"
+
 
 
 def main():
@@ -21,17 +26,33 @@ def main():
 
     print("* recording")
 
-    frames = queue.Queue()
-
+    #frames = queue.Queue()
+    frames = []
     try:
         while True:
             data = stream.read(CHUNK)
-            frames.put(data)
+            #frames.put(data)
+            frames.append(data)
     except KeyboardInterrupt:
+        frames = b''.join(frames)
         print('halted')
-        print(frames.get())
-
-    print(frames.get()) #for testing
+        fig = plt.figure()
+        s = fig.add_subplot(111)
+        amp = numpy.fromstring(frames, numpy.int16)
+        s.plot(amp)
+        plt.show()
+        #print(numpy.fromstring(frames.get(), numpy.dtype('Int16')))
+        #WAV conversion code
+        # stream.stop_stream()
+        # stream.close()
+        # p.terminate()
+        #
+        # wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        # wf.setnchannels(CHANNELS)
+        # wf.setsampwidth(p.get_sample_size(FORMAT))
+        # wf.setframerate(RATE)
+        # wf.writeframes(b''.join(frames))
+        # wf.close()
 
 
 if __name__ == "__main__":
--- gaussianadd.py
+++ gaussianadd.py
@@ -1,2 +1,4 @@
 #adds gaussian noise to input segment (mechanics tbd)
+#simulates Yi's program
 
+def
