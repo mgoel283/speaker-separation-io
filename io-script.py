d3b9821160b639adc3891b1f230243c9b4594c4c
mgoel283 <mgoel283@gmail.com>
2018-06-11 14:11:22 -0400

    added threads

--- io-script.py
+++ io-script.py
@@ -1,10 +1,9 @@
-#trying to write to thread safe queue
 import pyaudio
 import queue
-import wave
 import numpy
 import matplotlib.pyplot as plt
 import gaussianadd
+import threading
 
 
 CHUNK = 1024
@@ -14,7 +13,25 @@ RATE = 44100
 FORMAT = pyaudio.paInt16
 
 
+def get_input(): #figure out dropping frames
+    global STOP
+    while not STOP:
+        data = stream.read(CHUNK)
+        in_frames.put(data)
+
+def feed():
+    global STOP
+    while not STOP:
+        #give gaussianadd a frame
+        #write output frame to queue
+
+def play_out():
+    global STOP
+    while not STOP:
+        #play audio from output queue
+
 def main():
+    global STOP
     p = pyaudio.PyAudio()
 
     stream = p.open(format=FORMAT,
@@ -25,50 +42,33 @@ def main():
 
     print("* recording")
 
-    #frames = queue.Queue()
-    frames = []
-    try:
-        while True:
-            data = stream.read(CHUNK) #each data point is a bytes object
-            #frames.put(data)
-            frames.append(data)
-    except KeyboardInterrupt:
-        frames_temp = b''.join(frames) #bytes
-        # print('halted')
-        output_frames = []
-        # while not frames.empty():
-        #     output_frames.put(gaussianadd.add_gauss(frames.get(), CHUNK))
-        for i in range(0, len(frames)):
-            gauss_chunk = gaussianadd.add_gauss(numpy.fromstring(frames[i], numpy.int16), CHUNK)
-            output_frames.append(gauss_chunk)
-        #Printing waveform for testing
-        fig = plt.figure()
-        s = fig.add_subplot(211)
-        amp = numpy.fromstring(frames_temp, numpy.int16) #ndarray
-        s.plot(amp)
-        s2 = fig.add_subplot(212)
-        s2.plot(output_frames)
-        plt.show()
+    #thread to write to queue using get_input
+    t_1_write = threading.Thread(target = get_input())
+    t_1_write.daemon = True
 
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+    #thread to feed packets to gaussianadd and store in new queue
+    t_2_write = threading.Thread(target=feed())
+    t_2_write.daemon = True
 
-        #WAV conversion code
-        wf = wave.open('original.wav', 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+    #thread to play output
+    t_3_write = threading.Thread(target=play_out())
+    t_3_write.daemon = True
 
-        wf = wave.open('output.wav', 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE*4)
-        wf.writeframes(b''.join(output_frames))
-        wf.close()
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            print('* halted')
+            STOP = True
+            exit(0)
 
 
 if __name__ == "__main__":
+
+    STOP = False
+    #collection queue
+    in_frames = queue.Queue()
+    #output queue
+    out_frames = queue.Queue()
+
     main()
