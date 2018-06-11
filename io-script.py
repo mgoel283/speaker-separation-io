876a8c97e5f2170233ba5085cb41104e4cfc04bf
mgoel283 <mgoel283@gmail.com>
2018-06-11 16:45:36 -0400

    implemented threads

--- io-script.py
+++ io-script.py
@@ -1,9 +1,11 @@
 import pyaudio
 import queue
-import numpy
-import matplotlib.pyplot as plt
+import numpy as np
 import gaussianadd
 import threading
+from threading import Timer
+import struct
+import time
 
 
 CHUNK = 1024
@@ -19,40 +21,43 @@ def get_input(): #figure out dropping frames
         data = stream.read(CHUNK)
         in_frames.put(data)
 
+
 def feed():
     global STOP
     while not STOP:
-        #give gaussianadd a frame
-        #write output frame to queue
+        out_frames.put(gaussianadd.add_gauss(np.fromstring(in_frames.get(), np.int16), CHUNK))
+
 
 def play_out():
     global STOP
     while not STOP:
-        #play audio from output queue
+        samples = out_frames.get()
+        #samp_write = struct.pack('%dh' % len(samples), *samples)
+        stream2.write(samples)
+
 
 def main():
     global STOP
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
 
     print("* recording")
 
     #thread to write to queue using get_input
-    t_1_write = threading.Thread(target = get_input())
+    t_1_write = threading.Thread(target=get_input)
     t_1_write.daemon = True
+    t_1_write.start()
 
     #thread to feed packets to gaussianadd and store in new queue
-    t_2_write = threading.Thread(target=feed())
+    t_2_write = threading.Thread(target=feed)
     t_2_write.daemon = True
+    t_2_write.start()
 
     #thread to play output
-    t_3_write = threading.Thread(target=play_out())
-    t_3_write.daemon = True
+    # t_3_write = threading.Thread(target=play_out)
+    # t_3_write.daemon = True
+    # t_3_write.start()
+    t=Timer(5, play_out) #is choppy
+    t.start()
+
 
     while True:
         try:
@@ -70,5 +75,20 @@ if __name__ == "__main__":
     in_frames = queue.Queue()
     #output queue
     out_frames = queue.Queue()
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=CHUNK)
+
+    stream2 = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE*4,
+                    output=True,
+                    frames_per_buffer=CHUNK)
 
     main()
